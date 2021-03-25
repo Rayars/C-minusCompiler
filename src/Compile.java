11a5b2fa20cbb1db3d4e1f8da339eea68fe7c67f
Rayars <liumingxuan597@gmail.com>
2021-03-25 23:51:07 +0800

    项目提交

---
+++ src/Compile.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import CminusCompiler.getToken;
+
+public class Compile{
+    public static void main(String[] args) throws IOException {
+        System.out.println("test");
+    }
+}
---
+++ src/getToken.java
@@ -0,0 +1,5 @@
+public class getToken {
+    public String getToken(){
+
+    }
+}
