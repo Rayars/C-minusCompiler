1bcf717cee62c03e0c3d3a8a5ad20a791a7c38ec
Rayars <liumingxuan597@gmail.com>
2021-04-01 11:32:56 +0800

    项目提交

---
+++ src/functions/function.java
@@ -0,0 +1,11 @@
+package functions;
+
+public class function {
+    public static boolean isalpha(char c){
+        if(Character.isLetter(c)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
--- src/functions/getToken.java
+++ src/functions/getToken.java
@@ -1,16 +1,41 @@
 package functions;
+
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PushbackInputStream;
 
+
 public class getToken {
-    public static StringBuilder readToken(){
+    public static final int START=-1;
+    public static final int DONE=0;
+    public static final int ERROR=1;
+    public static final int INNUM=2;
+    public static final int INID=3;
+    public static final int INASSIGN=4;
+    public static final int INCOMMENT=5;
+
+    public static StringBuilder readToken() throws IOException {
         StringBuilder token=new StringBuilder(10);
-        PushbackInputStream input=new PushbackInputStream(System.in);
-        int state=1;
-        char nextCh='a';
-        while(state!=0&&state!=-1){
+        FileReader input=new FileReader("test.txt");
+        int state=START;
+        char Ch= (char) input.read();
+        while(state!=DONE){
             switch(state){
-                case 1:
+                case START:
+                    if(Character.isLetter(Ch)){
+                        state=INID;
+                    }else if(Character.isDigit(Ch)){
+                        state=INNUM;
+                    }else if(Ch==':'){
+
+                    }else{
+                        state=DONE;
+                        switch (Ch){
+                            case
+                        }
+                    }
+
+
                     try{
                         nextCh=(char)input.read();
                     }catch(IOException e){
