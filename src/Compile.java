a25ca5c5418a06573abcdfd88dd033976372b2db
Rayars <liumingxuan597@gmail.com>
2021-04-07 16:56:40 +0800

    项目提交

--- src/Compile.java
+++ src/Compile.java
@@ -1,9 +1,14 @@
-import java.io.*;
-import functions.*;
+import java.io.FileReader;
+import  java.io.IOException;
 
 public class Compile{
     public static void main(String[] args) throws IOException {
-        System.out.println(getToken.readToken());
-
+        //System.out.println(getToken.readToken());
+        FileReader input=new FileReader("C:\\Users\\Rayars\\Desktop\\C-minusCompiler\\src\\test.txt");
+        int Ch=' ';
+        while((Ch=input.read())!=-1){
+            System.out.print((char)Ch);
+        }
+        System.out.println(Character.isDigit(52));
     }
 }
--- src/functions/getToken.java
+++ src/functions/getToken.java
@@ -15,52 +15,81 @@ public class getToken {
     public static final int INCOMMENT=5;
 
     public static StringBuilder readToken() throws IOException {
-        StringBuilder token=new StringBuilder(10);
-        FileReader input=new FileReader("test.txt");
-        int state=START;
-        char Ch= (char) input.read();
-        while(state!=DONE){
-            switch(state){
-                case START:
-                    if(Character.isLetter(Ch)){
-                        state=INID;
-                    }else if(Character.isDigit(Ch)){
-                        state=INNUM;
-                    }else if(Ch==':'){
-
-                    }else{
-                        state=DONE;
-                        switch (Ch){
-                            case
-                        }
-                    }
-
-
-                    try{
-                        nextCh=(char)input.read();
-                    }catch(IOException e){
-                        e.printStackTrace();
-                    }
-                    if(nextCh=='a'){//状态转移对应的字符是‘a’
-                        token.append(nextCh);//nextCh收入token
-                        state=2;
-                    }else{
-                        try {
-                            input.unread(nextCh);//nextCh退回输入流
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        state=-1;
-                    }
-                    break;
+        FileReader input = new FileReader("C:\\Users\\Rayars\\Desktop\\C-minusCompiler\\src\\test.txt");
+        int state = START;
+        boolean unget = false;
+        int beforeCh = 0;
+        boolean save = true;
+        StringBuilder currentToken = new StringBuilder(10);
+        while (state != DONE) {
+            int Ch = 0;
+            if (!unget) {
+                Ch=input.read();
+            } else {
+                Ch = beforeCh;
             }
-            if(state==0){
-                return token;
-            }else{
-                //error
+            unget = false;
+            switch (state) {
+                case START:
+                    if (Character.isLetter(Ch)) {
+                        state = INID;
+                    } else if (Character.isDigit(Ch)) {
+                        state = INNUM;
+                    } else if (Ch == ':') {//赋值
+                        state = INASSIGN;
+                    } else {
+                        state = DONE;
+                        switch (Ch) {
+                            case -1:
+                                break;//同EOF
+                            case '+':
+                                currentToken.append('+');
+                                break;
+                            case '-':
+                                currentToken.append('-');
+                                break;
+                            case '*':
+                                currentToken.append('*');
+                                break;
+                            case '/':
+                                currentToken.append('/');
+                                break;
+                            default:
+                                currentToken.append("error");
+                                state = ERROR;
+                                break;
+                        }
+                    }
+                case INID:
+                    if (Character.isLetter(Ch) || Character.isDigit(Ch)) {
+                        currentToken.append((char)Ch);
+                        state = INID;
+                    } else {
+                        unget = true;
+                        beforeCh = Ch;
+                        state = DONE;//字符回退
+                    }
+                case INNUM:
+                    if(Character.isDigit(Ch)){
+                        currentToken.append((char)Ch);
+                        state=INNUM;
+                    }else if(Character.isLetter(Ch)){
+                        state=ERROR;
+                    }else{
+                        unget = true;
+                        beforeCh = Ch;
+                        state = DONE;//字符回退
+                    }
+                case INASSIGN:
+
+                case INCOMMENT:
+
+                case ERROR:
+                    save=false;
+                    currentToken.delete(0,currentToken.length());
+                case DONE:
             }
         }
-        token.append(1);
-        return token;
+        return currentToken;
     }
 }
