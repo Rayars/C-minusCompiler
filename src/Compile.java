1bacb8c0840c977f83826cf7dc74d698a9c485fa
Rayars <liumingxuan597@gmail.com>
2021-04-10 18:43:03 +0800

    基本完成，错误处理待完成

--- src/Compile.java
+++ src/Compile.java
@@ -1,14 +1,10 @@
 import java.io.FileReader;
 import  java.io.IOException;
+import functions.getToken;
 
 public class Compile{
     public static void main(String[] args) throws IOException {
-        //System.out.println(getToken.readToken());
-        FileReader input=new FileReader("C:\\Users\\Rayars\\Desktop\\C-minusCompiler\\src\\test.txt");
-        int Ch=' ';
-        while((Ch=input.read())!=-1){
-            System.out.print((char)Ch);
-        }
-        System.out.println(Character.isDigit(52));
+        getToken gt=new getToken();
+        gt.readToken("C:\\Users\\Rayars\\Desktop\\C-minusCompiler\\src\\test.txt");
     }
 }
--- src/functions/getToken.java
+++ src/functions/getToken.java
@@ -3,6 +3,8 @@ package functions;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PushbackInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class getToken {
@@ -13,83 +15,208 @@ public class getToken {
     public static final int INID=3;
     public static final int INASSIGN=4;
     public static final int INCOMMENT=5;
+    public static final int INCOMP=6;
+    public static final int ID=7;
+    public static final int NUM=8;
+    public static final List<String> reserved=new ArrayList<String>();
 
-    public static StringBuilder readToken() throws IOException {
-        FileReader input = new FileReader("C:\\Users\\Rayars\\Desktop\\C-minusCompiler\\src\\test.txt");
-        int state = START;
+    public getToken(){
+        reserved.add("int");
+        reserved.add("float");
+        reserved.add("long");
+        reserved.add("if");
+        reserved.add("else");
+        reserved.add("return");
+        reserved.add("void");
+    }
+
+    public static void readToken(String fileName) throws IOException {
+        FileReader input = new FileReader(fileName);
+        boolean EOF=false;
         boolean unget = false;
         int beforeCh = 0;
-        boolean save = true;
-        StringBuilder currentToken = new StringBuilder(10);
-        while (state != DONE) {
-            int Ch = 0;
-            if (!unget) {
-                Ch=input.read();
-            } else {
-                Ch = beforeCh;
-            }
-            unget = false;
-            switch (state) {
-                case START:
-                    if (Character.isLetter(Ch)) {
-                        state = INID;
-                    } else if (Character.isDigit(Ch)) {
-                        state = INNUM;
-                    } else if (Ch == ':') {//赋值
-                        state = INASSIGN;
-                    } else {
-                        state = DONE;
-                        switch (Ch) {
-                            case -1:
-                                break;//同EOF
-                            case '+':
-                                currentToken.append('+');
-                                break;
-                            case '-':
-                                currentToken.append('-');
-                                break;
-                            case '*':
-                                currentToken.append('*');
-                                break;
-                            case '/':
-                                currentToken.append('/');
-                                break;
-                            default:
-                                currentToken.append("error");
-                                state = ERROR;
-                                break;
+        while(!EOF) {
+            StringBuilder currentToken = new StringBuilder(10);
+            int TokenType=0;
+            int state = START;
+            boolean save = true;
+            while (state != DONE) {
+                int Ch = 0;
+                if (!unget) {
+                    Ch = input.read();
+                } else {
+                    Ch = beforeCh;
+                    unget = false;
+                }
+                switch (state) {
+                    case START:
+                        if (Character.isLetter(Ch)) {
+                            currentToken.append((char) Ch);
+                            state = INID;
+                        } else if (Character.isDigit(Ch)) {
+                            currentToken.append((char)Ch);
+                            state = INNUM;
+                        } else if (Ch == ':') {//赋值
+                            currentToken.append((char)Ch);
+                            state = INASSIGN;
+                        } else if (Ch == '/') {
+                            currentToken.append((char)Ch);
+                            state = INCOMMENT;//包括了除法的情况
+                        } else if (Ch == '<' || Ch == '>' || Ch == '=' || Ch == '!') {
+                            currentToken.append((char)Ch);
+                            state = INCOMP;
+                        } else if (Ch == 32 || Ch == 13||Ch==10) {
+                            state = START;//读入空格或回车忽略
+                        } else {
+                            state = DONE;
+                            switch (Ch) {
+                                case -1:
+                                    EOF = true;
+                                    break;//同EOF
+                                case '+':
+                                    currentToken.append('+');
+                                    break;
+                                case '-':
+                                    currentToken.append('-');
+                                    break;
+                                case '*':
+                                    currentToken.append('*');
+                                    break;
+                                case '(':
+                                    currentToken.append('(');
+                                    break;
+                                case ')':
+                                    currentToken.append(')');
+                                    break;
+                                case '[':
+                                    currentToken.append('[');
+                                    break;
+                                case ']':
+                                    currentToken.append(']');
+                                    break;
+                                case '{':
+                                    currentToken.append('{');
+                                    break;
+                                case '}':
+                                    currentToken.append('}');
+                                    break;
+                                case ';':
+                                    currentToken.append(';');
+                                    break;
+                                case ',':
+                                    currentToken.append(',');
+                                    break;
+                                default:
+                                    state = ERROR;
+                                    break;
+                            }
                         }
-                    }
-                case INID:
-                    if (Character.isLetter(Ch) || Character.isDigit(Ch)) {
-                        currentToken.append((char)Ch);
-                        state = INID;
-                    } else {
-                        unget = true;
-                        beforeCh = Ch;
-                        state = DONE;//字符回退
-                    }
-                case INNUM:
-                    if(Character.isDigit(Ch)){
-                        currentToken.append((char)Ch);
-                        state=INNUM;
-                    }else if(Character.isLetter(Ch)){
-                        state=ERROR;
+                        break;
+                    case INID:
+                        if (Character.isLetter(Ch) || Character.isDigit(Ch)) {
+                            currentToken.append((char) Ch);
+                            state = INID;
+                        } else {
+                            unget = true;
+                            beforeCh = Ch;
+                            TokenType=ID;
+                            state = DONE;//字符回退
+                        }
+                        break;
+                    case INNUM:
+                        if (Character.isDigit(Ch)) {
+                            currentToken.append((char)Ch);
+                            state = INNUM;
+                        } else if (Character.isLetter(Ch)) {
+                            state = ERROR;
+                        } else {
+                            unget = true;
+                            beforeCh = Ch;
+                            TokenType=NUM;
+                            state = DONE;//字符回退
+                        }
+                        break;
+                    case INASSIGN:
+                        if (Ch == '=') {
+                            currentToken.append(Ch);
+                            state = DONE;
+                        } else {
+                            unget = true;
+                            beforeCh = Ch;
+                            state = ERROR;
+                        }
+                        break;
+                    case INCOMMENT:
+                        int sub_state = 0;
+                        while (state != DONE) {
+                            switch (sub_state) {
+                                case 0:
+                                    if (Ch == '*') {
+                                        sub_state = 1;
+                                        currentToken.delete(0, 1);//确定为注释，将之前放入的'/'删除
+                                        Ch = input.read();
+                                    } else {
+                                        unget = true;
+                                        beforeCh = Ch;
+                                        state = DONE;
+                                    }
+                                    break;
+                                case 1:
+                                    if (Ch == '*') {
+                                        sub_state = 2;
+                                        Ch = input.read();
+                                    } else {
+                                        sub_state = 1;
+                                        Ch = input.read();
+                                    }
+                                    break;
+                                case 2:
+                                    if (Ch == '/') {
+                                        state = DONE;
+                                    } else if (Ch == '*') {
+                                        sub_state = 2;
+                                        Ch = input.read();
+                                    } else {
+                                        sub_state = 1;
+                                        Ch = input.read();
+                                    }
+                                    break;
+                            }
+                        }
+                        break;
+                    case INCOMP:
+                        if (Ch == '=') {
+                            currentToken.append('=');
+                            state = DONE;
+                        } else {
+                            unget = true;
+                            beforeCh = Ch;
+                            state = DONE;
+                        }
+                        break;
+                    case ERROR:
+                        save = false;
+                        currentToken.delete(0, currentToken.length());
+                        currentToken.append("ERROR!");
+                        break;
+                    case DONE:
+                        save = true;
+                        break;
+                }
+            }
+            if (save == true&&currentToken.length()!=0) {
+                if (TokenType == ID) {
+                    if(reserved.contains(currentToken.toString())){
+                        System.out.println(currentToken);
                     }else{
-                        unget = true;
-                        beforeCh = Ch;
-                        state = DONE;//字符回退
+                        System.out.println("id,"+currentToken);
                     }
-                case INASSIGN:
-
-                case INCOMMENT:
-
-                case ERROR:
-                    save=false;
-                    currentToken.delete(0,currentToken.length());
-                case DONE:
+                }else if(TokenType==NUM){
+                    System.out.println("num,"+currentToken);
+                }else{
+                    System.out.println(currentToken);
+                }
             }
         }
-        return currentToken;
     }
 }
