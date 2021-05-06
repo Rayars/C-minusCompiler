4058a068338f3a7c09c49beda37c5b2cd4db4751
Rayars <liumingxuan597@gmail.com>
2021-05-06 23:19:15 +0800

    语法分析-递归下降

--- src/functions/SyntexAnalysis.java
+++ src/functions/SyntexAnalysis.java
@@ -10,18 +10,30 @@ public class SyntexAnalysis {
 
     public SyntexAnalysis() throws IOException {
         Tokens=new getToken("C:\\Users\\27545\\IdeaProjects\\C-minusCompiler\\src\\test.txt");
+        currentToken=Tokens.readToken();
     }
 
-    public void match(StringBuilder Vn){
+    public void match(StringBuilder Vn) throws IOException{
+        if(Vn==currentToken){
+            currentToken=Tokens.readToken();
+        }else{
+            error();
+        }
+    }
+
+    public void error(){
 
     }
 
-    public void program(){
+    public void program() throws IOException{
         stmt_sequence();
     }
 
-    public void stmt_sequence(){
+    public void stmt_sequence() throws IOException{
         statement();
+        StringBuilder Vt=new StringBuilder("10");
+        Vt.append(";");
+        match(Vt);
 
     }
 
